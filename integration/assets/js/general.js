fb89c5117f5241d9d4c2b9e19b33f732b3dd0583
Dan Anghel <dan.anghel89@gmail.com>
2022-12-09 22:49:41 +0100

    fix 12/09/22

---
+++ integration/assets/js/general.js
@@ -0,0 +1,36 @@
+// general scripts
+
+$(function() {
+	var clicked_time = 0;
+
+	$('#sort-items').on('click', function() {
+		clicked_time++;
+
+		$('#widget-items .col-lg').each(function() {
+			var position = 'order-' + $(this).attr('position');
+
+			if ( clicked_time % 2 == 0 ) {
+				$(this).removeClass(position);
+
+
+				$('.sort-asc').addClass('hidden');
+				$('.sort-alpha').removeClass('hidden');
+
+			} else {
+				$(this).addClass(position);
+
+				$('.sort-alpha').addClass('hidden');
+				$('.sort-asc').removeClass('hidden');
+			}
+		});
+	});
+
+	$('#change-view').on('click', function() {
+		if ( $(this).is(':checked') ) {
+			$('#widget-items').addClass('vertical');
+
+		} else {
+			$('#widget-items').removeClass('vertical');
+		}
+	});
+});
